37e5700cee338e5d8a67fea0ebf848c27e721e69
David Herrig <david.herrig@gmail.com>
2023-12-06 13:32:21 -0700

    WIP schedule simulation for yahoo league

---
+++ sim_schedules.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+
+import argparse
+import functools
+import itertools
+import math
+import sys
+from collections import defaultdict
+import tqdm
+import warnings
+
+import pandas as pd
+import yahoo
+
+
+def main(argv=None):
+  if argv is None:
+    argv = sys.argv[1:]
+  parser = get_parser()
+  parser = yahoo_parser(parser)
+  args = parser.parse_args(argv)
+
+  print('Connecting to yahoo api')
+  creds = yahoo.obtain_credentials(args, yahoo.YahooFantasyOAuth.required_creds)
+  auth = yahoo.YahooFantasyOAuth(creds['client_id'], creds['client_secret'],
+                                 force_refresh_token=args.force_refresh_token)
+  fantasyapi = yahoo.YahooFantasyAPI(auth)
+  myleague = yahoo.YahooLeagueResource(fantasyapi, game_key='nfl', league_id=args.league_id)
+  myteams = myleague.teams
+  print(f'We made {fantasyapi.query_count} queries so far')
+
+  print('Creating Dataframe of all scores')
+  teams_df, scores_df, num_weeks = dfs_from_yahoo(myteams, maxweek=args.weeks)
+  print(f'We made {fantasyapi.query_count} queries so far')
+
+  simmer = ScheduleSimmer(teams_df, scores_df, num_weeks)
+
+  print('Simulating schedules')
+  team_keys = list(teams_df.index)
+  all_records = simmer.simulate_all_schedules(team_keys)
+  print('ALL RECORDS:')
+  for team_key_i in all_records.keys():
+    print(teams_df.loc[team_key_i])
+    print_records_dict(all_records[team_key_i])
+
+
+def dfs_from_yahoo(yh_teams: list[yahoo.YahooTeamResource], maxweek=None):
+  """Return dataframes containing team details and scores"""
+  # teams_df columns: team key, team name, manager name
+  # scores_df columns: team key, week, points
+  team_keys = [x.team_key for x in yh_teams]
+  team_names = [x.name for x in yh_teams]
+  # mgr_names = [[y.name for y in x.managers] for x in yh_teams]
+  mgr_names = ['NA' for _ in yh_teams]  # remove manager names for now to reduce number of queries. they are all returning 'hidden' for some reason
+  teams_index = pd.Index(team_keys, name='team_key')
+  teams_df = pd.DataFrame(zip(team_names, mgr_names), index=teams_index, columns=['team_name', 'manager_name'])
+  scores_lst = []
+
+  for i, team_i in enumerate(yh_teams):
+    team_i_scores = [y.points for y in team_i.scores]
+    if (i == 0) and (maxweek is None):
+      maxweek = len(team_i_scores)
+    team_i_scores = team_i_scores[:maxweek]
+    scores_lst.extend(team_i_scores)
+
+  week_nums = range(1, 1 + maxweek)
+  scores_index = pd.MultiIndex.from_product([team_keys, week_nums], names=['team_key', 'week_num'])
+  scores_df = pd.DataFrame(scores_lst, index=scores_index, columns=['points'])
+  return teams_df, scores_df, maxweek
+
+
+class ScheduleSimmer:
+  def __init__(self,
+               teams_df: pd.DataFrame,
+               scores_df: pd.DataFrame,
+               num_weeks: int) -> None:
+    self.teams_df = teams_df
+    self.scores_df = scores_df
+    self.nweeks = num_weeks
+    self.nteams = len(self.teams_df)
+
+  @functools.cache
+  def _compute_h2h_result_cache(self,
+                                team_a_key: str,
+                                team_b_key: str,
+                                week_n: int) -> tuple[bool, bool, bool]:
+    """
+    Return the head to head result as if team_a played team_b in week_n.
+
+    For more compact caching, teamid_a and teamid_b should be passed in sorted order, teamid_a < teamid_b.
+
+    Returns:
+      (bool, bool, bool): (team a wins, team b wins, tie). Only one item should be True in each return result.
+    """
+    team_a_score = get_week_score(self.scores_df, team_a_key, week_n)
+    team_b_score = get_week_score(self.scores_df, team_b_key, week_n)
+    team_a_win = team_a_score > team_b_score
+    teamab_draw = team_a_score == team_b_score
+    team_b_win = not (teamab_draw or team_a_win)
+    return team_a_win, team_b_win, teamab_draw
+
+  def compute_h2h_result(self,
+                         team_a_key: str,
+                         team_b_key: str,
+                         week_n: int) -> tuple[bool, bool, bool]:
+    if team_a_key > team_b_key:
+      x = self._compute_h2h_result_cache(team_b_key, team_a_key, week_n)
+      return x[1], x[0], x[2]
+    else:
+      return self._compute_h2h_result_cache(team_a_key, team_b_key, week_n)
+
+  def simulate_all_schedules(self, team_keys: list[str]) -> dict:
+    all_records = {x: defaultdict(int) for x in team_keys}
+    total_schedules = math.factorial(self.nteams - 1)
+    for team_i_key in team_keys:
+      all_schedules_iter = permute_schedules(team_i_key, team_keys)
+      print(f'Simulating {total_schedules} schedules for team {team_i_key}')
+      with tqdm.tqdm(total=total_schedules) as pbar:
+        for j, sched_j in enumerate(all_schedules_iter):
+          # print(f'{j} of {total_schedules}')
+          # print(team_i_key, sched_j)
+          record_j = self.compute_record(team_i_key, sched_j)
+          # print(record_j)
+          all_records[team_i_key][record_j] += 1
+          if j % 100 == 0:
+            pbar.update(100)
+      print(team_i_key)
+      print_records_dict(all_records[team_i_key], self.nweeks)
+    return all_records
+
+  def compute_record(self,
+                     team_key: str,
+                     schedule):
+    num_otherteams = self.nteams - 1
+    n_win = 0
+    n_loss = 0
+    n_draw = 0
+    for week_idx in range(self.nweeks):
+      week_num = week_idx + 1
+      week_idx_wrap = week_idx % num_otherteams
+      other_team_key = schedule[week_idx_wrap]
+      win_i, loss_i, draw_i = self.compute_h2h_result(team_key, other_team_key, week_num)
+      n_win += win_i
+      n_loss += loss_i
+      n_draw += draw_i
+    return n_win, n_loss, n_draw
+
+
+def print_records_dict(d, nweeks):
+  # first print all keys
+  print('all nonzero records in dict:')
+  for k in sorted(d.keys()):
+    print(f'{k}: {d[k]}')
+  # then print all possible w/l records assuming zero ties:
+  print('all possible w/l records assuming no ties:')
+  for nwins in range(nweeks + 1):
+    nloss = nweeks - nwins
+    k = (nwins, nloss, 0)
+    print(f'{k}: {d[k]}')
+
+
+def get_week_score(scores_df, team_key, week):
+  return scores_df.loc[(team_key, week)]['points']
+
+
+def permute_schedules(teamid, allteamids):
+  """
+  Return an iterable giving all permutations of a list of n teams, excluding teamid.
+
+  Args:
+    teamid (int or str): id of the team we are generating the schedule for
+    allteamids (list or iterable object): list of all teamids in the league
+
+  Returns:
+
+  """
+  opponents = list(allteamids)
+  opponents.remove(teamid)
+  return itertools.permutations(opponents)
+
+
+def get_parser():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--league', '-l', default=None)
+  parser.add_argument('--weeks', '-n', default=None, type=int)
+  return parser
+
+
+def yahoo_parser(parser: argparse.ArgumentParser | None = None) -> argparse.ArgumentParser:
+  """Create and return argument parser."""
+  if parser is None:
+    parser = argparse.ArgumentParser()
+  parser.add_argument('--league_id', type=str)
+  parser.add_argument('--client_id', type=str)
+  parser.add_argument('--client_secret', type=str)
+  parser.add_argument('--redirect_uri', type=str, default='oob')
+  parser.add_argument('--force_refresh_token', action='store_true')
+  return parser
+
+
+if __name__ == '__main__':
+  main()
+
+
+# todos:
+# implement logging by importing same settings from yahoo module
+# maybe replace use of 'warnings' module with logger.warn messages
+# implement full schedule generation in addition to
+# for better efficiency, maybe organize team scores into a dataframe first instead of querying from each team each time
+# implement loading private credentials from file instead of passing by argument
--- yahoo.py
+++ yahoo.py
@@ -11,13 +11,21 @@
 # NOTE some of the auth and token urls in yahoo tutorials seem wrong or out of
 # date. This last oauth2 guide from yahoo has the correct newest urls
 
+from __future__ import annotations  # allows use of "forward annotations" of undefined classes within type hints
 import argparse
+from decimal import Decimal
+import enum
+import functools
 import json
+import logging
 import os
 import sys
 import time
+from typing import Optional
 import warnings
+import xml.etree.ElementTree as ET
 
+import requests
 import requests_oauthlib
 
 
@@ -25,10 +33,47 @@ _OAUTH_GET_TOKEN_URL = 'https://api.login.yahoo.com/oauth2/get_token'
 _OAUTH_REFRESH_TOKEN_URL = 'https://api.login.yahoo.com/oauth2/get_token'
 _OAUTH_REQUEST_AUTH_URL = 'https://api.login.yahoo.com/oauth2/request_auth'
 _TMP_DIR = '/tmp/'
-# _LEAGUE_KEYS = {'2023': 423, '2022': 414, '2021': 406, '2020': 399}
+_XMLNS = 'http://fantasysports.yahooapis.com/fantasy/v2/base.rng'
+_NS = {'xmlns': _XMLNS}
+
+logger = logging.getLogger(__name__)
 
 
-def make_parser():
+def main(argv=None):
+  """Main function, only run if executed as script. Not run if module is imported."""
+  if argv is None:
+    argv = sys.argv[1:]
+  setup_log_handlers()
+  parser = make_parser()
+  args = parser.parse_args(argv)
+  creds = obtain_credentials(args, YahooFantasyOAuth.required_creds)
+  auth = YahooFantasyOAuth(creds['client_id'], creds['client_secret'],
+                           force_refresh_token=args.force_refresh_token)
+  fantasyapi = YahooFantasyAPI(auth)
+  myleague = YahooLeagueResource(fantasyapi, game_key='nfl', league_id=args.league_id)
+  myteams = myleague.teams
+  print(myteams[0].name)
+  print(myteams[0].get_week_score(3))
+  print(myteams[0].get_week_score(2))
+  print(myteams[0].managers)
+  # myleague = fantasyapi.get_league(game_key='nfl', league_id=args.league_id)
+  # print(type(myleague.raw))
+  # myleague.get_teams()
+  print(fantasyapi.query_count)
+
+
+def setup_log_handlers():
+  global logger
+  logger.setLevel(logging.DEBUG)
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  fh = logging.FileHandler('yahoo_log.txt', encoding='utf-8')
+  fh.setLevel(logging.DEBUG)
+  fh.setFormatter(formatter)
+  logger.addHandler(fh)
+
+
+def make_parser() -> argparse.ArgumentParser:
+  """Create and return argument parser."""
   parser = argparse.ArgumentParser()
   parser.add_argument('--league_id', type=str)
   parser.add_argument('--client_id', type=str)
@@ -38,23 +83,214 @@ def make_parser():
   return parser
 
 
-def main(argv=None):
-  if argv is None:
-    argv = sys.argv[1:]
-  parser = make_parser()
-  args = parser.parse_args(argv)
-  creds = obtain_credentials(args)
-  x = YahooOAuth(creds['client_id'], creds['client_secret'], args.league_id,
-                 force_refresh_token=args.force_refresh_token)
-  x.test_auth()
+class YahooFantasyAPI:
+  _BASE_URL = 'https://fantasysports.yahooapis.com/fantasy/v2'
+
+  def __init__(self, oauth_client) -> None:
+    self.oauth_client = oauth_client
+    self.query_count = 0  # keep track of number of non-cached queries
+
+  def get_resource(self, resource_name, resource_key) -> requests.Response:
+    """request a Resource as defined at https://developer.yahoo.com/fantasysports/guide/#resources-and-collections"""
+    uri = f'{resource_name}/{resource_key}'
+    return self.get_uri(uri)
+
+  @functools.cache
+  def _get_uri_cached(self, uri) -> requests.Response:
+    return self._get_uri_live(uri)
+
+  def _get_uri_live(self, uri) -> requests.Response:
+    url = f'{self._BASE_URL}/{uri}'
+    self.query_count += 1
+    return self.oauth_client.get(url)
+
+  def get_uri(self, uri, bypasscache=False) -> requests.Response:
+    if bypasscache:
+      return self._get_uri_live(uri)
+    else:
+      return self._get_uri_cached(uri)
+
+  def __repr__(self) -> str:
+    return f'YahooFantasyAPI({repr(self.oauth_client)})'
 
 
-class YahooOAuth:
-  def __init__(self, client_id, client_secret, league_id, redirect_uri=None, force_refresh_token=False):
+class YahooManager:
+  def __init__(self,
+               team: YahooTeamResource,
+               manager_id: str,
+               name: str,
+               guid: str) -> None:
+    self.manager_id = manager_id
+    self.name = name
+    self.guid = guid
+    self.team = team
+
+  def __eq__(self, other):
+    return isinstance(other, YahooManager) and (self.guid == other.guid)
+
+  @property
+  def nickname(self):
+    return self.name
+
+  @nickname.setter
+  def nickname(self, value):
+    self.name = value
+
+  def __repr__(self) -> str:
+    return f'YahooManager({repr(self.team)}, {repr(self.manager_id)}, {repr(self.name)}, {repr(self.guid)})'
+
+
+class YahooResourceXML:
+  def __init__(self, resource_name, resource_key) -> None:
+    self.resource_name = resource_name
+    self.resource_key = resource_key
+
+  @property
+  def raw(self):
+    return self.api.get_resource(self.resource_name, self.resource_key).text
+
+  def __eq__(self, other) -> bool:
+    return isinstance(other, YahooResourceXML) and (other.resource_key == self.resource_key)
+
+  def fetch_tag(self, s):
+    root = ET.fromstring(self.raw)
+    node = root.find(f'./xmlns:{self.resource_name}', _NS)
+    tag_vals = xml_elementtree_todict(node)
+    return tag_vals[s]
+
+
+class YahooTeamResource(YahooResourceXML):
+  def __init__(self,
+               api: YahooFantasyAPI,
+               parent_league: YahooLeagueResource,
+               team_key: str,
+               **kwargs) -> None:
+    self.api = api
+    self.league = parent_league
+    self.team_key = team_key
+    self._props = kwargs
+    self._managers = None
+    self._scores = None
+    super().__init__('team', self.team_key)
+
+  def __repr__(self) -> str:
+    return f'YahooTeamResource({repr(self.api)}, {repr(self.league)}, {repr(self.team_key)}, {repr(self.name)})'
+
+  @property
+  def name(self):
+    _name = self._props.get('name', None)
+    if _name is None:
+      _name = self.fetch_tag('name')
+      self._props['name'] = _name
+    return _name
+
+  @property
+  def managers(self):
+    if self._managers is None:
+      uri = f'team/{self.team_key}'
+      s = self.api.get_uri(uri).text
+      root = ET.fromstring(s)
+      _managers = []
+      for manager_node_i in root.findall('./xmlns:team/xmlns:managers/xmlns:manager', _NS):
+        manager_vals_i = xml_elementtree_todict(manager_node_i)
+        manager_guid_i = manager_vals_i['guid']
+        manager_id_i = manager_vals_i['manager_id']
+        manager_name_i = manager_vals_i['nickname']
+        manager_i = YahooManager(self, manager_id_i, manager_name_i, manager_guid_i)
+        _managers.append(manager_i)
+      self._managers = _managers
+    return self._managers
+
+  def __eq__(self, other) -> bool:
+    return isinstance(other, YahooTeamResource) and (other.team_key == self.team_key)
+
+  @property
+  def scores(self):
+    uri = f'team/{self.team_key}/matchups'
+    s = self.api.get_uri(uri, bypasscache=True).text  # bypass cache to make sure we get latest live score
+    root = ET.fromstring(s)
+    _scores = []
+    for matchup_node_i in root.findall('./xmlns:team/xmlns:matchups/xmlns:matchup', _NS):
+      # each matchup contains two teams
+      # As a side effect to looking up this team, we're going to get a bunch of other teams' scores
+      # I could do something useful with them... but I ignore them here
+      team_points_i = None
+      team_projected_points_i = None
+      matchup_vals_i = xml_elementtree_todict(matchup_node_i)
+      week_i = matchup_vals_i['week']
+      status_i = MatchupStatus.from_yahoostatus(matchup_vals_i['status'])
+      team_nodes = matchup_node_i.findall('./xmlns:teams/xmlns:team', _NS)
+      for team_node_j in team_nodes:
+        team_key_j = team_node_j.find('xmlns:team_key', _NS).text
+        if team_key_j != self.team_key:
+          continue
+        team_points_j = Decimal(team_node_j.find('./xmlns:team_points/xmlns:total', _NS).text)
+        team_projected_points_j = Decimal(team_node_j.find('./xmlns:team_projected_points/xmlns:total', _NS).text)
+        team_points_i = team_points_j
+        team_projected_points_i = team_projected_points_j
+        break
+      # finally:
+      matchup_score_i = TeamMatchupScore(self, int(week_i), status_i, team_points_i, team_projected_points_i)
+      _scores.append(matchup_score_i)
+    return _scores
+
+  def get_week_score(self, week: int) -> list[TeamMatchupScore]:
+    """Return this teams' scores in given weeks"""
+    week_idx = week - 1
+    return self.scores[week_idx]
+
+class YahooLeagueResource(YahooResourceXML):
+  def __init__(self,
+               api: YahooFantasyAPI,
+               league_id: int,
+               game_key: str,
+               league_key: Optional[str] = None):
+    self.api = api
+    self.league_id = league_id
+    self.game_key = game_key
+    if league_key is None:
+      league_key = f'{game_key}.l.{league_id}'
+    self.league_key = league_key
+    super().__init__('league', self.league_key)
+    self._teams = None
+
+  @property
+  def raw(self):
+    return self.api.get_resource('league', self.league_key).text
+
+  def __repr__(self) -> str:
+    s = f'YahooLeagueResource({repr(self.api)}, {repr(self.league_id)}, {repr(self.game_key)}, {repr(self.league_key)})'
+    return s
+
+  @property
+  def teams(self) -> list[YahooTeamResource]:
+    if self._teams is None:
+      _teams = []
+      uri = f'league/{self.league_key}/teams'
+      s = self.api.get_uri(uri).text
+      root = ET.fromstring(s)
+      for team_node_i in root.findall('./xmlns:league/xmlns:teams/xmlns:team', _NS):
+        team_vals_i = xml_elementtree_todict(team_node_i)
+        team_key_i = team_vals_i['team_key']
+        team_name_i = team_vals_i['name']
+        team_i = YahooTeamResource(self.api, self, team_key_i, name=team_name_i)
+        _teams.append(team_i)
+      self._teams = _teams
+    return self._teams
+
+
+class YahooGameResource(YahooResourceXML):
+  def __init__(self, game_key):
+    self.game_key = game_key
+    super().__init__('game', self.game_key)
+
+
+class YahooFantasyOAuth:
+  required_creds = {'client_id', 'client_secret', 'redirect_uri'}
+
+  def __init__(self, client_id, client_secret, redirect_uri=None, force_refresh_token=False):
     self.client_id = client_id
     self.client_secret = client_secret
-    self.league_id = league_id
-    self.league_url = f'https://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.{self.league_id}'
     if redirect_uri is None:
       redirect_uri = 'oob'
     self.redirect_uri = redirect_uri
@@ -65,13 +301,19 @@ class YahooOAuth:
     else:
       self.update_client()
 
-  def get(self, url):
-    return self.client.get(url)
+  def __repr__(self) -> str:
+    return f'YahooFantasyOAuth({repr(self.client_id)}, {repr(self.client_secret)}, {repr(self.redirect_uri)})'
 
-  def update_client(self, expires_at=None):
+  def get(self, url: str) -> requests.Response:
+    resp = self.client.get(url)
+    if resp.status_code != 200:
+      raise RuntimeError(f'get{url} returned status {resp.status_code} instead of 200.'
+                         ' Full response text:\n' + resp.text)
+    return resp
+
+  def update_client(self, expires_at=None) -> None:
     if self.token is None:
       self.load_token()
-    print(self.token)
     self.update_token_expiration(force_value=expires_at)
     extra = {
         'client_id': self.client_id,
@@ -84,18 +326,17 @@ class YahooOAuth:
                                                   auto_refresh_kwargs=extra,
                                                   token_updater=self.token_updater
                                                   )
-    self.test_auth()
 
-  def token_updater(self, token):
-    print('token updater was called')
+  def token_updater(self, token) -> None:
+    logger.debug('token updater was called')
     self.token = token
     self.save_token()
 
-  def save_token(self):
+  def save_token(self) -> None:
     token_path = get_token_filepath(self.client_id)
     with open(token_path, 'w', encoding="utf-8") as f:
       json.dump(self.token, f)
-    print(f'[token saved to {token_path}]')
+    logger.debug(f'token saved to {token_path}')
 
   def load_token(self):
     token_path = get_token_filepath(self.client_id)
@@ -111,9 +352,9 @@ class YahooOAuth:
       print(f'file at {token_path} is not valid json.')
       os.remove(token_path)
     else:
-      print(f'loaded token file at {token_path}')
+      logger.debug('loaded token file at {token_path}')
     if not success:
-      print('Fetching a new token.')
+      logger.debug('Fetching a new token.')
       self.get_new_token()
 
   def get_new_token(self):
@@ -126,7 +367,6 @@ class YahooOAuth:
     self.token = self.client.fetch_token(_OAUTH_GET_TOKEN_URL,
                                          client_secret=self.client_secret,
                                          code=auth_code)
-    self.test_auth()
     self.save_token()
 
   def update_token_expiration(self, force_value=None):
@@ -143,32 +383,100 @@ class YahooOAuth:
                     f' to {next_expires_in}')
     self.token['expires_in'] = next_expires_in
     self.token['expires_at'] = next_expires_at
-    print(f'[token expires_in updated from {last_expires_in} to {next_expires_in}]')
-    print(f'[token expires_at updated from {last_expires_at} to {next_expires_at}]')
+    logger.debug(f'token expires_in updated from {last_expires_in} to {next_expires_in}')
+    logger.debug(f'token expires_at updated from {last_expires_at} to {next_expires_at}')
     self.save_token()
 
-  def test_auth(self):
-    """Verify auth by fetching a protected url"""
-    resp = self.get(self.league_url)
-    if resp.status_code != 200:
-      raise RuntimeError(f'Fetching {self.league_url} returned status {resp.status_code}'
-                         f' instead of 200. Text:' + '\n' + resp.text)
-    # todo: handle errors. maybe try refreshing token
+#  TODO: refactor YahooFantasyOauth to use a token class, like I started below
+# class YahooOauthToken:
+#   def __init__(self,
+#                client_id: str,
+#                value: Optional[dict] = None) -> None:
+#     self.client_id = client_id
+#     self.value = value
+
+#   @property
+#   def filepath(self):
+#     return
+
+#   def save(self):
+#     raise NotImplementedError
+
+#   def load(self):
+#     if self.value is not None:
+#       raise ValueError('Loading new token from disk will overwrite existing value!')
+#     raise NotImplementedError
+
+#   def update_expiration(self):
+#     raise NotImplementedError
 
 
-def league_api_url_currentyear(league_id):
-  return f'https://fantasysports.yahooapis.com/fantasy/v2/league/nfl.l.{league_id}'
+class YahooMatchup:
+  def __init__(self,
+               week: str | int,
+               team1: YahooTeamResource,
+               team2: YahooTeamResource) -> None:
+    self.week = week
+    teams_sorted = sorted([team1, team2])
+    self.team_a = teams_sorted[0]
+    self.team_b = teams_sorted[1]
+    self.teams = set(teams_sorted)
+
+  def __eq__(self, other) -> bool:
+    return (set(self.teams) == set(other.teams)) and (self.week == other.week)
 
 
-def get_token_filepath(client_id):
+class MatchupStatus(enum.Enum):
+  PREEVENT = 1
+  MIDEVENT = 2
+  POSTEVENT = 3
+
+  @classmethod
+  def from_yahoostatus(cls, s: str) -> MatchupStatus:
+    return cls[s.upper()]
+
+
+class TeamMatchupScore:
+  def __init__(self,
+               team: YahooTeamResource,
+               week: str | int,
+               status: MatchupStatus,
+               points: Decimal,
+               projected_points: Decimal) -> None:
+    self.team = team
+    self.week = week
+    self.status = status
+    self.points = points
+    self.projected_points = projected_points
+
+  @property
+  def complete(self) -> bool:
+    return self.status == MatchupStatus.POSTEVENT
+
+  @property
+  def inprogress(self) -> bool:
+    return self.status == MatchupStatus.INPROGRESS
+
+  @property
+  def future(self) -> bool:
+    return self.status == MatchupStatus.PREEVENT
+
+  def __str__(self) -> str:
+    return f'Team {self.team.name} week {self.week} ({self.status}) score: {self.points} actual vs {self.projected_points} projected'
+
+  def __repr__(self) -> str:
+    return self.__str__()
+
+
+def get_token_filepath(client_id: str | int) -> str:
   return os.path.join(_TMP_DIR, f'oauth2_token_{client_id}.json')
 
 
-def obtain_credentials(args):
+def obtain_credentials(args, required_creds):
   """Obtain the client_id, client_secret, and redirect_uri credentials manually if not passed as args."""
   creds = {}
   args_dict = vars(args)
-  for cred_label in ('client_id', 'client_secret', 'redirect_uri'):
+  for cred_label in required_creds:
     if cred_label in args_dict:
       this_cred = args_dict[cred_label]
     else:
@@ -187,5 +495,26 @@ def manual_cred_input(label):
   return new_cred
 
 
+def xml_elementtree_todict(node, strip_ns=_XMLNS):
+  """Convert xml to dict where each tag is a key and text is value and namespace is stripped from keys."""
+  d = {}
+  for child in node:
+    if strip_ns:
+      k = child.tag.removeprefix(f'{{{strip_ns}}}')
+    else:
+      k = child.tag
+    d[k] = child.text.strip()
+  return d
+
+
 if __name__ == '__main__':
   main()
+
+
+# todos
+# don't cache gets if they relate to any mid-event scoring
+# add sub-resources of team like matchups, stats (by season or by date.. does the api allow filtering stats by week?)
+# todo: add sub-resources of league: /settings, /standings, /scoreboard
+# write lt/gt/etc functions to allow teams to be sorted (by team key)
+# maybe split yahoo api and yahoo resources into separate files
+# maybe write some methods for the api that automatically convert the whole xml stuff to a json. Then the yahoo api file could become a standalone command line executable of some sort that returns json
